db14145a46bd5cc6943f7873d4f74eeb0df1008e
vincent <495285586@qq.com>
2018-08-27 10:19:47 +0800

    up

--- 位运算/231. Power of Two.py
+++ 位运算/231. Power of Two.py
@@ -21,6 +21,9 @@ Input: 218
 Output: false
 
 二进制表示只有一个 1 存在。
+
+如果一个数是2的次方数的话，根据上面分析，那么它的二进数必然是最高位为1，
+其它都为0，那么如果此时我们减1的话，则最高位会降一位，其余为0的位现在都为变为1，那么我们把两数相与，就会得到0，用这个性质也能来解题
 """
 
 class Solution:
@@ -30,3 +33,11 @@ class Solution:
         :rtype: bool
         """
         return n > 0 and list(bin(n)).count('1') == 1
+
+class Solution1:
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and not n & (n - 1)
